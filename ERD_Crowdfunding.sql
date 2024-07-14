2f6d226c0b097c72063159caac3c03ea16402d7d
gunelq <gunelka91@gmail.com>
2024-07-14 02:08:32 -0500

    ERD diagram was updated

---
+++ ERD_Crowdfunding.sql
@@ -0,0 +1,66 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/DJ9VAd
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "contacts" (
+    "contact_id" INTEGER   NOT NULL,
+    "first_name" VARCHAR(150)   NOT NULL,
+    "last_name" VARCHAR(150)   NOT NULL,
+    "email" VARCHAR(150)   NOT NULL,
+    "last_updated" TIMESTAMP Default Localtimestamp  NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "Category" (
+    "category_id" VARCHAR   NOT NULL,
+    "category" VARCHAR   NOT NULL,
+    "last_updated" TIMESTAMP Default Localtimestamp  NOT NULL,
+    CONSTRAINT "pk_Category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "SubCategory" (
+    "subcategory_id" VARCHAR   NOT NULL,
+    "subcategory" VARCHAR   NOT NULL,
+    "last_updated" TIMESTAMP Default Localtimestamp  NOT NULL,
+    CONSTRAINT "pk_SubCategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "crowdfunding" (
+    "cf_id" INTEGER   NOT NULL,
+    "contact_id" INTEGER   NOT NULL,
+    "company_name" VARCHAR   NOT NULL,
+    "blurb" VARCHAR   NOT NULL,
+    "goal" INTEGER   NOT NULL,
+    "pledged" INTEGER   NOT NULL,
+    "outcome" VARCHAR(50)   NOT NULL,
+    "backers_count" INTEGER   NOT NULL,
+    "country" VARCHAR   NOT NULL,
+    "currency" VARCHAR   NOT NULL,
+    "launched_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "staff_pick" BOOLEAN   NOT NULL,
+    "spotlight" BOOLEAN   NOT NULL,
+    "last_updated" TIMESTAMP Default Localtimestamp  NOT NULL,
+    "category_id" VARCHAR   NOT NULL,
+    "subcategory_id" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_crowdfunding" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+ALTER TABLE "Category" ADD CONSTRAINT "fk_Category_category_id" FOREIGN KEY("category_id")
+REFERENCES "crowdfunding" ("category_id");
+
+ALTER TABLE "SubCategory" ADD CONSTRAINT "fk_SubCategory_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "crowdfunding" ("subcategory_id");
+
+ALTER TABLE "crowdfunding" ADD CONSTRAINT "fk_crowdfunding_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
